9f4043de3e26d4462508e6bbb0ddd57f69b83d1b
Scott Turro <turroscott@gmail.com>
2020-07-20 18:37:41 -0500

    init CI-publish branch

---
+++ Tests/test_app_full.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+from TestRun import runTest
+
+
+"""
+Copyright 2020 University of Illinois Board of Trustees.
+Licensed under the terms of an MIT license
+"""
+
+__author__ = 'Quoleon/Turro'
+"""CHECK OUT THE REFERENCE PAGE ON OUR WEBSITE :
+http://research.physics.illinois.edu/QI/Photonics/Quantum-Tomography_lib_Ref/"""
+
+
+def simple_general_test():
+
+    assert(runTest([1, 0, 0, 0, 0, 0, 0, 50]) == 1 )
+    assert(runTest([1, 3, 0, 0, 0, 0, 0, 50]) == 1 )
+    assert(runTest([1, 0, 0, 1, 0, 0, 0, 50]) == 1 )
+    assert(runTest([1, 0, 0, 0, 1, 0, 0, 50]) == 1 )
+    assert(runTest([1, 0, 0, 0, 0, 0, 1, 50]) == 1 )
+
+    assert(runTest([2, 0, 0, 0, 0, 0, 0, 10]) == 1 )
+    assert(runTest([2, 3, 0, 0, 0, 0, 0, 10]) == 1 )
+    assert(runTest([2, 0, 1, 0, 0, 0, 0, 10]) == 1 )
+    assert(runTest([2, 0, 0, 1, 0, 0, 0, 10]) == 1 )
+    assert(runTest([2, 0, 0, 0, 1, 0, 0, 10]) == 1 )
+    assert(runTest([2, 0, 0, 0, 0, 1, 0, 10]) == 1 )
+    assert(runTest([2, 0, 0, 0, 0, 0, 1, 10]) == 1 )
+
+    assert(runTest([3, 0, 0, 0, 0, 0, 0, 1]) == 1 )
+
+simple_general_test()
