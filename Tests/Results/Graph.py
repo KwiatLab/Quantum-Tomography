7fe15ac5a5917dae071d9c58a20db8e3061b8490
Scott Turro <turroscott@gmail.com>
2020-07-30 12:23:29 -0500

    Descriptive stats now added to results grapher script

--- Tests/Results/Graph.py
+++ Tests/Results/Graph.py
@@ -1,20 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from QuantumTomography import floatToString
 
-"""
-Copyright 2020 University of Illinois Board of Trustees.
-Licensed under the terms of an MIT license
-"""
 
-__author__ = 'Quoleon/Turro'
-"""CHECK OUT THE REFERENCE PAGE ON OUR WEBSITE :
-http://research.physics.illinois.edu/QI/Photonics/Quantum-Tomography_lib_Ref/"""
-
-"""This script is used to graph the data created by the tests"""
-
-maxNumberOfQubits = 1
+maxNumberOfQubits = 3
 timeVsCounts_xVals = []
 timeVsCounts_yVals = []
+# For descriptive stats at bottom of image
+p = np.array([['nBits',"nStates",'Mean',"std"],['nQ',"nStates",'Mean',"std"],['nQ',"nStates",'Mean',"std"],['nQ',"nStates",'Mean',"std"]],dtype="O")
 
 for x in range(1,maxNumberOfQubits+1):
 
@@ -25,7 +18,10 @@ for x in range(1,maxNumberOfQubits+1):
 
     timeVsCounts_xVals = np.append(timeVsCounts_xVals,x*np.ones_like(groupData[2]))
     timeVsCounts_yVals = np.append(timeVsCounts_yVals,groupData[2])
-
+    p[x, 0] = x
+    p[x, 1] = len(groupData[2])
+    p[x, 2] = np.mean(groupData[2])
+    p[x, 3] = np.std(groupData[2])
     fig = plt.figure()
     fig.clf()
     plt.plot(np.log10(groupData[0]),groupData[1],'.b',label="Our Group's Tomography")
@@ -58,4 +54,24 @@ plt.legend(loc='upper left')
 plt.title('Time')
 plt.xlabel("nQubits")
 plt.ylabel("Time (sec)")
+fig.subplots_adjust(bottom=0.3)
+# Adds descriptive stats at bottom of image
+
+mx = 0
+descriptiveStats=""
+for i in range(p.shape[0]):
+    for j in range(p.shape[1]):
+        if(not isinstance(p[i, j],str)):
+            p[i, j] = floatToString(p[i, j]).replace(" ", "") + "  "
+        if(len(p[i, j])>mx):
+            mx = len(p[i, j])
+
+for i in range(p.shape[0]):
+    for j in range(p.shape[1]):
+        descriptiveStats += p[i, j] + " "*(mx-len(p[i, j]))
+    descriptiveStats += "\n"
+
+
+plt.figtext(0.2, 0.02, descriptiveStats, wrap=True, horizontalalignment='left', fontsize=12, fontname='monospace')
+
 plt.savefig("timeGraph_NQUBITS.png")
