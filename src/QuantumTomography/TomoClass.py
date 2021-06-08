4668f9a4e5fa5460d7b22f6b4efa5b0c5b4448b2
shallat3 <joeyshallat@gmail.com>
2021-06-08 11:24:02 -0500

    Moved the comment format comment to the __init__.py file in the QuantumTomography folder.

--- src/QuantumTomography/TomoClass.py
+++ src/QuantumTomography/TomoClass.py
@@ -13,32 +13,6 @@ __author__ = 'Quoleon/Turro'
 """CHECK OUT THE REFERENCE PAGE ON OUR WEBSITE :
 http://research.physics.illinois.edu/QI/Photonics/Quantum-Tomography_lib_Ref/"""
 
-'''
-    Comments Should be formatted like the following: 
-    (this comment is written in single quotes so that it is not picked up by the documentation generator).
-    
-    function(parameter 1, parameter 2)
-    Desc: This is where the description is written.
-
-    Parameters
-    ----------      (10 hyphens, one for each letter)
-    parameter 1 : type
-    description
-    parameter 2 : type
-    description
-                    (make sure the white spaces between are deleted all the way to the left side of the screen)
-    Returns
-    -------     (7 hyphens, one for each letter)
-    returnValue 1 : type
-    description
-    returnValue 2 : type
-    description
-
-    See Also
-     ------   (space, 6 hyphens, another space)
-    function1;function2;function2      (these should be the functionTitle without parentheses, as it appears on the Table of Contents
-'''
-
 """
     class Tomography()
     Desc: This is the main tomography object that the library is built around. The goal is to only have one tomography object and edit the
