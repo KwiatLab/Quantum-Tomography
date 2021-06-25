e2fd3dbcacf88fba4ed72c6c348632f71cae3f8f
Scott Turro <smturro2@illinois.edu>
2021-06-25 10:06:01 -0500

    Bayesian now incorporates drift correction

--- src/QuantumTomography/TomoClassHelpers.py
+++ src/QuantumTomography/TomoClassHelpers.py
@@ -199,13 +199,15 @@ def independent_set(measurements):
             break
     return s
 
-# This function converts a list of loglikelihoods to normalized likelihoods
-def normalizeExponentLikelihood(likelihoods):
-    nFactor = min(likelihoods)
+# This function converts a list of loglikelihoods to normalized likelihoods. Also returns the index of
+# the min loglike
+def normalizeLikelihoods(likelihoods):
+    nIndex = np.argmin(likelihoods)
+    nFactor = likelihoods[nIndex]
     likelihoods = likelihoods - nFactor
     likelihoods = np.exp(-1 * likelihoods)
-    likelihoods /= sum(likelihoods)
-    return likelihoods
+    # likelihoods /= sum(likelihoods)
+    return likelihoods, nIndex
 
 
 
@@ -214,17 +216,21 @@ log_likelyhood(t, coincidences, accidentals, m, prediction)
 Desc: This is the log 
 Parameters
 ----------
-t : ndarray
-    T values of the current predicted state.
+intensity : float
+    The overall intensity of the measurments
+givenState : 1darray with length 2^(2*numQubits) or ndarray with shape = (2^numQubits,2^numQubits)
+    T values of the current predicted state. A density matrix can also be used.
 coincidences : ndarray with length = number of measurements or shape = (number of measurements, 2^numQubits) for 2 det/qubit
     The counts of the tomography.
+measurements : ndarray with shape = (number of measurements,2^numQubits, 2^numQubits)
+    The measurements of the tomography in density matrix form.
 accidentals : ndarray with length = number of measurements or shape = (number of measurements, 2^numQubits) for 2 det/qubit
     The singles values of the tomography. Used for accidental correction.
-m : ndarray with shape = (2^numQubits, 2^numQubits, number of measurements)
-    The measurements of the tomography in density matrix form.
 prediction : ndarray
     Predicted counts from the predicted state.
-
+overall_norms : 1darray with length = number of measurements or length = number of measurements * 2^numQubits for 2 det/qubit. (optional)
+    The relative weights of each measurment. Used for drift correction.
+    
 Returns
 -------
 val : float
@@ -236,13 +242,13 @@ def log_likelyhood(intensity, givenState, coincidences, measurements, accidental
         overall_norms = np.ones(coincidences.shape[0])
     elif not (len(overall_norms.shape) == 1 and overall_norms.shape[0] == coincidences.shape[0]):
         raise ValueError("Invalid intensities array")
+    # Convert to densities if given tvals
     if (len(givenState.shape) == 1):
         givenState = t_to_density(givenState)
+    # Calculate excpected averages
     Averages = np.zeros_like(coincidences, dtype=np.float)
     for j in range(len(Averages)):
-        Averages[j] = intensity * overall_norms[j] * np.real(
-            np.trace(np.matmul(measurements[:, :, j], givenState))) + \
-                      accidentals[j]
+        Averages[j] = intensity * overall_norms[j] * np.real(np.trace(np.matmul(measurements[j], givenState))) + accidentals[j]
         # Avoid dividing by zero for pure states
         if (Averages[j] == 0):
             Averages[j] == 1
@@ -259,7 +265,6 @@ def b_matrix(projectors):
         for j in range(dim_b):
             tmp[i][j] = np.inner(projectors[i], np.inner(sigma_n(j, dim_m), projectors[i].conj().transpose()))
     b = tmp.transpose()
-
     return b
 
 
