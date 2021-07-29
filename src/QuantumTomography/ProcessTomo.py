9556d7435747bfc159ee3564453c20d1f4a2dd64
shallat3 <joeyshallat@gmail.com>
2021-07-29 13:13:18 -0500

    Wrote entire process tomography algorithm in the package. Added some comments, still need to go through and add tests though.

---
+++ src/QuantumTomography/ProcessTomo.py
@@ -0,0 +1,132 @@
+import numpy as np
+import scipy
+from .ProcessTomoHelpers import *
+from .TomoClassHelpers import make_positive
+from .TomoFunctions import density2t, t_to_density
+
+"""
+Copyright 2020 University of Illinois Board of Trustees.
+Licensed under the terms of an MIT license
+"""
+
+__author__ = 'Quoleon/Turro'
+"""CHECK OUT THE REFERENCE PAGE ON OUR WEBSITE :
+http://research.physics.illinois.edu/QI/Photonics/Quantum-Tomography_lib_Ref/"""
+
+
+"""
+ProcessTomography(input_states, measurement_states, coincidences)
+Desc: This function calculates the chi matrix most likely to have produced the observed counts
+
+Parameters
+----------
+coincidences : ndarray with shape = (number of measurements, number of measurements)
+    Each column corresponds to an input state, and each row corresponds to a measurement state.
+input_states : ndarray with shape = (number of measurements, 2) or (number of measurements, number of measurements)
+    Each row is a pure state used as an input to the process.
+measurement_states : ndarray with shape = (number of measurements, 2)
+    Each row is a pure state used to measure the state after the process.
+    
+Returns
+-------
+chi_matrix : ndarray with shape = (4,4)
+    Represents the chi matrix that best characterizes the process.
+"""
+def ProcessTomography(coincidences, input_states=get_default_states(6), measurement_states=get_default_states(6)):
+
+    # using tomography to calculate the density matrices after the process, and converting the input pure states (or coincidences) and measurement pure states to density matrices
+    input_densities, measurement_densities, output_densities = get_process_densities(coincidences,
+                                                                                           input_states,
+                                                                                           measurement_states)
+
+    # construct the starting chi matrix to be input into the MLE process tomography
+    chi_matrix = StandardProcessTomography(input_densities, measurement_densities, output_densities)
+
+    # makes the chi matrix positive definite so that the cholesky decomp can be used in MLE
+    chi_matrix = make_positive(chi_matrix)
+
+    # performs the MLE on the chi matrix
+    chi_matrix = MLEProcessTomography(chi_matrix, input_densities, measurement_densities, coincidences)
+
+    return chi_matrix
+
+# TODO: finish this comment
+"""
+MLEProcessTomography(chi_matrix), coincidences, input_densities, measurement_densities)
+Desc:
+
+"""
+def MLEProcessTomography(chi_matrix, coincidences, input_densities, measurement_densities):
+    #converting the chi matrix to its t values so that there are less parameters for optimization
+    chi_matrix_t_vals = density2t(chi_matrix)
+    chi_matrix_t_vals += 0.0001
+
+    # get the final t values from scipy.optimize.least sq on the process_count_differences function
+    final_chi_t_vals = scipy.optimize.leastsq(process_count_differences, chi_matrix_t_vals, args=(input_densities, measurement_densities, coincidences))[0]
+
+    # converting the t values back into a chi matrix and then normalizing
+    chi_matrix = t_to_density(final_chi_t_vals)
+    chi_matrix = chi_matrix / np.trace(chi_matrix)
+
+    return chi_matrix
+
+
+"""
+StandardProcessTomography(input_densities, measurement_densities, output_densities)
+Desc: Uses Standard Process Tomography (see Joe Altepeter's thesis pg. 69) to calculate a likely chi matrix for a given process.
+
+Parameters
+----------
+input_densities : dictionary with number of elements = number of input states
+    A dictionary (with integer keys starting at 0), with each value representing a density matrix for an input.
+measurement_densities : dictionary with number of elements = number of measurements
+    A dictionary (again with integer keys starting at 0) with each value representing the density matrix of a measurement state
+output_densities : dictionary with number of elements = number of input states
+    A dictionary in which each value represents the output for the corresponding input in the input_densities dictionary.
+    
+Returns
+-------
+chi_matrix : ndarray with shape = (4, 4)
+    The chi matrix that characterizes the process.
+"""
+def StandardProcessTomography(input_densities, measurement_densities, output_densities):
+    # get the number of measurements
+    n_measurements = len(measurement_densities.keys())
+
+    # c matrix definted in eq. 4.52 of Joe Altepeter's thesis page 69
+    c_matrix = get_c_matrix(output_densities, measurement_densities)
+
+    # B matrix defined in eq. 4.53 of Joe Altepeter's thesis page 69
+    b_matrix = get_b_matrix(input_densities, measurement_densities)
+
+    # constructing the chi matrix from the B and c matrices in eq. 4.55 pg. 60 of Joe Altepeter's thesis
+    chi_matrix = construct_chi(b_matrix, c_matrix)
+
+    return chi_matrix
+
+
+"""
+post_process_ensity(chi_matrix, rho)
+Desc: Calculates the post-process density matrix of a given input rho through a process characterized by the chi matrix
+
+Parameters
+----------
+chi_matrix : ndarray with shape = (4, 4)
+    The chi matrix that characterizes the process to send the input through.
+rho : ndarray with shape = (2, 2)
+    Density matrix representing the input state to the process.
+    
+Returns
+-------
+output_rho : ndarray with shape = (4, 4)
+    The density matrix output from the process.
+"""
+def post_process_density(chi_matrix, rho):
+    output_rho = np.zeros_like(rho)
+    paulis = get_paulis()
+
+    for m in range(len(paulis.keys())):
+        for n in range(len(paulis.keys())):
+            output_rho += chi_matrix[m, n] * (paulis[m] @ rho @ paulis[n].conj().transpose())
+
+    return output_rho
---
+++ src/QuantumTomography/ProcessTomoHelpers.py
@@ -0,0 +1,165 @@
+import numpy as np
+import scipy
+from .TomoClass import Tomography
+from .TomoClassHelpers import t_to_density
+from .TomoFunctions import toDensity
+from .ProcessTomo import *
+
+"""
+Copyright 2020 University of Illinois Board of Trustees.
+Licensed under the terms of an MIT license
+"""
+
+__author__ = 'Quoleon/Turro'
+"""CHECK OUT THE REFERENCE PAGE ON OUR WEBSITE :
+http://research.physics.illinois.edu/QI/Photonics/Quantum-Tomography_lib_Ref/"""
+
+
+"""
+The function that is input for MLE Process Tomography.
+It produces the log likelihood function (before summing squares) of the count differences between observed and expected.
+"""
+def process_count_differences(chi_matrix_t_vals, input_densities, measurement_densities, coincidences):
+    n_measurements = coincidences.shape[0]
+
+    chi_matrix = t_to_density(chi_matrix_t_vals)
+    chi_matrix = chi_matrix / np.trace(chi_matrix)
+
+    counts_predicted = np.zeros_like(coincidences)
+
+    for i in range(n_measurements):
+        current_output_rho = post_process_density(chi_matrix, input_densities)
+        for j in range(n_measurements):
+            counts_predicted[j,i] = max(min(np.real(np.trace(measurement_densities[j] @ current_output_rho)),1),0)
+
+    # this is done so that the coincidences are on the same scale: so that they are all near each other.
+    norm_factor = np.average(coincidences) / np.average(counts_predicted)
+    counts_predicted *= norm_factor
+
+    log_like = (counts_predicted - coincidences) / np.sqrt(counts_predicted)
+
+    flattened_log_like = np.ndarray.flatten(log_like)
+
+    return flattened_log_like
+
+"""
+Returns the default linearly independent states that span the space for 4 measurements of 6 measurements (default).
+"""
+def get_default_states(num_measurements=6):
+    # H, V, D, A, R, L
+    default_states_6 = np.array([[1,0],[0,1],[0.7071,0.7071],[0.7071,-0.7071],[0.7071,0.7071j],[0.7071,-0.7071j]], dtype=complex)
+    # H, V, D, R
+    default_states_4 = np.array([[1,0],[0,1],[0.7071,0.7071],[0.7071,0.7071j]], dtype=complex)
+
+    if num_measurements == 6:
+        return default_states_6
+    elif num_measurements == 4:
+        return default_states_4
+    else:
+        raise ValueError("Default States are only defined for 6 and 4 measurements")
+
+"""
+Gets the density matrices of the inputs, outputs, and measurement states from the coincidences, 
+input states, and measurement states.
+"""
+def get_process_densities(coincidences, input_states, measurement_states):
+    input_densities = {}
+    measurement_densities = {}
+    output_densities = {}
+
+    n_measurements = coincidences.shape[0]
+
+    if input_states.shape == (n_measurements, 2):
+        for i in range(n_measurements):
+            input_densities = toDensity(input_states[i,:])
+    elif input_states.shape == (n_measurements, n_measurements):
+        for i in range(n_measurements):
+            #if the input states were given as counts, the input states are the columns, and the measurement states the rows
+            #so you take the state tomography of each column to give you the rho of each input state
+            input_densities[i] = Tomography(1).StateTomography(measurement_states, input_states[:,i])
+    else:
+        raise ValueError("input states must be mx2 matrix of pure states or mxm matrix of measurement counts")
+
+
+    for i in range(n_measurements):
+        measurement_densities[i] = toDensity(measurement_states[i,:])
+
+        output_densities[i] = Tomography(1).StateTomography(measurement_states, coincidences[:,i])
+
+    return input_densities, measurement_densities, output_densities
+
+"""
+gets c matrix from Joe Altepeter's Thesis page 69 eq. 4.52
+"""
+def get_c_matrix(output_densities, measurement_densities):
+    n_measurements = len(measurement_densities.keys())
+
+    # flattening the measurement densities into a single matrix so that the c matrix can be solved for.
+    # each column of this matrix is a 2x2 measurement density flattened into its 4 elements
+    flattened_measurements = np.zeros((4,n_measurements), dtype=complex)
+    for i in range(n_measurements):
+        flattened_measurements[:,i] = np.ndarray.flatten(measurement_densities[i])
+
+    c_matrix = np.zeros((n_measurements, n_measurements))
+
+    for i in range(n_measurements):
+        current_output_rho_flat = np.ndarray.flatten(output_densities[i])
+        c_matrix[i,:] = np.linalg.lstsq(flattened_measurements, current_output_rho_flat)
+
+    return c_matrix
+
+"""
+Gets B matrix from Joe Altepeter's thesis page 69 equation 4.53
+"""
+def get_b_matrix(input_densities, measurement_densities):
+    n_measurements = len(measurement_densities.keys())
+
+    # flattening the measurement densities into a single matrix so that the c matrix can be solved for.
+    # each column of this matrix is a 2x2 measurement density flattened into its 4 elements
+    flattened_measurements = np.zeros((4, n_measurements), dtype=complex)
+    for i in range(n_measurements):
+        flattened_measurements[:, i] = np.ndarray.flatten(measurement_densities[i])
+
+    b_matrix = np.zeros((4, 4, n_measurements, n_measurements), dtype=complex)
+    paulis = get_paulis()
+
+    # equation 4.53 in Joe Altepeter's thesis, solving for the elements of B
+    for m in range(4):
+        for n in range(4):
+            for j in range(n_measurements):
+                current_input = paulis[m] @ input_densities[j] @ paulis[n].conj().transpose()
+                current_input_flat = np.ndarray.flatten(current_input)
+
+                b_matrix[m, n, j, :] = np.linalg.lstsq(flattened_measurements, current_input_flat)
+
+    return b_matrix
+
+"""
+Constructs the chi matrix from the B and C matrices labeled above.
+"""
+def construct_chi(b_matrix, c_matrix):
+    n_measurements = b_matrix.shape[2]
+
+    inv_b = np.pinv(b_matrix)
+    chi_matrix = np.zeros((4,4), dtype=complex)
+
+    for m in range(4):
+        for n in range(4):
+            for j in range(n_measurements):
+                for k in range(n_measurements):
+                    chi_matrix[m,n] += inv_b[m,n,j,k] * c_matrix[j,k]
+
+    chi_matrix = chi_matrix / np.trace(chi_matrix)
+    return chi_matrix
+
+"""
+Returns the pauli matrices for use in Process Tomography
+"""
+def get_paulis():
+    default_paulis = {
+        0: np.array([[1, 0], [0, 1]], dtype=complex),
+        1: np.array([[0, 1], [1, 0]], dtype=complex),
+        2: np.array([[0, -1j], [1j, 0]], dtype=complex),
+        3: np.array([[1, 0], [0, -1]], dtype=complex)
+    }
+    return default_paulis
