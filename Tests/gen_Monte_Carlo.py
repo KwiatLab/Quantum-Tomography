58fc3a2a961d104544f7e69c65048ea68d11a6b2
Scott Turro <turroscott@gmail.com>
2021-07-30 12:20:08 -0500

    got rid of author

--- Tests/gen_Monte_Carlo.py
+++ Tests/gen_Monte_Carlo.py
@@ -8,7 +8,7 @@ Copyright 2020 University of Illinois Board of Trustees.
 Licensed under the terms of an MIT license
 """
 
-__author__ = 'Quoleon/Turro'
+
 
 
 """CHECK OUT THE REFERENCE PAGE ON OUR WEBSITE :
