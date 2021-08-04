cfe853e0c59b015dcfc27c501b58717bfaad3012
shallat3 <joeyshallat@gmail.com>
2021-08-04 10:51:42 -0500

    Got rid of author in Process Tomography files.

--- src/QuantumTomography/ProcessTomo.py
+++ src/QuantumTomography/ProcessTomo.py
@@ -9,7 +9,6 @@ Copyright 2020 University of Illinois Board of Trustees.
 Licensed under the terms of an MIT license
 """
 
-__author__ = 'Quoleon/Turro'
 """CHECK OUT THE REFERENCE PAGE ON OUR WEBSITE :
 http://research.physics.illinois.edu/QI/Photonics/Quantum-Tomography_lib_Ref/"""
 
