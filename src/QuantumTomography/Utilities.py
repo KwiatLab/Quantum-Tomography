9d13013a62c294d79516e6d3a3329e7cc4fefc9a
timur <16829159+timurjavid@users.noreply.github.com>
2025-06-20 22:14:48 -0500

    start finalized format work

--- src/QuantumTomography/Utilities.py
+++ src/QuantumTomography/Utilities.py
@@ -9,7 +9,7 @@ import json
 import tomllib
 import numpy as np
 from pydantic import BaseModel, model_validator
-from typing import Union, List, Tuple
+from typing import Union, List, Tuple, get_type_hints, Type, Dict, Any
 from typing_extensions import Self
 
 """
@@ -17,7 +17,7 @@ Copyright 2020 University of Illinois Board of Trustees.
 Licensed under the terms of an MIT license
 """
 
-
+POLARIZATION_STATE_NAMES = ["H","V","D","A","R","L"]
 class Polarizations(Enum):
     H = auto()
     V = auto()
@@ -25,7 +25,6 @@ class Polarizations(Enum):
     A = auto()
     R = auto()
     L = auto()
-
 
 POLARIZATION_STATES = {
     Polarizations.H: np.array([1, 0], dtype=np.complex128),
@@ -57,6 +56,9 @@ TOMOGRAPHY_TYPES = {
 
 
 class TomoConfiguration(BaseModel):
+    class Config:
+        arbitrary_types_allowed = True
+
     use_derivative: bool = False
     get_bell_settings: bool = False
     do_error_estimation: bool = False
@@ -69,7 +71,13 @@ class TomoConfiguration(BaseModel):
     maxfev: Union[int, None] = None
     method: Union[TomographyType, str] = TomographyType.MLE
     starting_matrix: Union[np.ndarray, None] = None
-    save_state = True
+    save_state: bool = True
+    minimizer_kwargs: Dict = {
+        "ftol": ftol,
+        "xtol": xtol,
+        "gtol": gtol,
+        "maxfev": maxfev,
+    }
 
     @model_validator(mode="after")
     def check_method(self) -> Self:
@@ -91,29 +99,86 @@ class TomoData(BaseModel):
     n_qubits: int = 1
     n_detectors: int = 1
     n_measurements_per_qubit: int = 6
-    coincidence_window: float = 0.0
-    rel_efficiency: List[float] = [0, 0, 0, 0]
+    rel_efficiency: np.ndarray = np.array([1])
     crosstalk: np.ndarray = np.zeros((2**n_qubits, 2**n_qubits))
-    measurement_densities: np.ndarray = np.array(
-        [density for _, density in POLARIZATION_DENSITIES.items()]
-    )
-    counts: np.ndarray = np.zeros((n_qubits**n_detectors, n_measurements_per_qubit))
+    measurement_densities: Dict[str,np.ndarray] = {name: POLARIZATION_DENSITIES[i] for i, name in enumerate(POLARIZATION_STATE_NAMES)}
+    data: List[Dict] = [
+        {"basis": [POLARIZATION_STATE_NAMES[0]],
+        "integration_time": 1,
+        "counts": [50]},
+
+        {"basis": [POLARIZATION_STATE_NAMES[1]],
+        "integration_time": 1,
+        "counts": [50]},
+
+        {"basis": [POLARIZATION_STATE_NAMES[2]],
+        "integration_time": 1,
+        "counts": [50]},
+
+        {"basis": [POLARIZATION_STATE_NAMES[3]],
+        "integration_time": 1,
+        "counts": [50]},
+
+        {"basis": [POLARIZATION_STATE_NAMES[4]],
+        "integration_time": 1,
+        "counts": [100]},
+
+        {"basis": [POLARIZATION_STATE_NAMES[5]],
+        "integration_time": 1,
+        "counts": [0]},
+    ]
+    coincidence_window: np.ndarray = np.zeros((n_detectors,n_detectors))
     intensity: np.ndarray = np.ones(len(measurement_densities))
-    overall_norms: np.ndarray = np.kron(intensity, rel_efficiency)
-    accidentals: np.ndarray = np.zeros_like(counts)
+    overall_norms: np.ndarray = np.kron(rel_efficiency, intensity)
+    accidentals: np.ndarray = np.zeros_like(singles)
+
+    # @model_validator(mode="after")
+    # def check_input_shape(self) -> Self:
+    # self.counts = np.atleast_2d(np.array(self.counts, dtype=np.complex128))
+    # if self.counts.shape != (
+    #    self.n_qubits**self.n_detectors,
+    #    self.n_measurements_per_qubit,
+    # ):
+    #    raise ValueError(
+    #        f"Counts shape is wrong {self.counts.shape}, expected {self.n_qubits**self.n_detectors, self.n_measurements_per_qubit}"
+    #    )
+
+    #    return self
 
     @model_validator(mode="after")
-    def check_input_shape(self) -> Self:
-        self.counts = np.atleast_2d(np.array(self.counts, dtype=np.complex128))
-        if self.counts.shape != (
-            self.n_qubits**self.n_detectors,
-            self.n_measurements_per_qubit,
-        ):
-            raise ValueError(
-                f"Counts shape is wrong {self.counts.shape}, expected {self.n_qubits**self.n_detectors, self.n_measurements_per_qubit}"
-            )
+    def make_window_symmetric(self)->Self:
 
+
+    @model_validator(mode="after")
+    def cast_lists_to_array(self) -> Self:
+        self.measurement_densities = np.array(self.measurement_densities)
+        self.rel_efficiency = np.array(self.rel_efficiency)
         return self
+
+    @model_validator(mode="after")
+    def accidental_correction(self) -> Self:
+        if self.config.do_accidental_correction == 1:
+            scalerIndex = np.concatenate((np.ones(nbits - 2), [2, 2]))
+            additiveIndex = np.array([0, 1])
+            for j in range(2, nbits):
+                additiveIndex = np.concatenate(([2 * j], additiveIndex))
+            if len(coinc.shape) == 1:
+                acc = acc[:, np.newaxis]
+            for j in range(n_coinc):
+                index = bin(j).split("b")[1]
+                index = "0" * (nbits - len(index)) + index
+                index = [int(char) for char in index]
+                index = index * scalerIndex + additiveIndex
+                index = np.array(index, dtype=int)
+                acc[:, j] = np.prod(np.real(sings[:, tuple(index)]), axis=1) * (
+                    window[j] * 1e-9 / np.real(times)
+                ) ** (nbits - 1)
+            if acc.shape != coinc.shape:
+                acc = acc[:, 0]
+
+
+def cast_to_numpy(json_dict, key):
+    json_dict[key] = np.array(json_dict[key], dtype=np.complex128)
 
 
 """CHECK OUT THE REFERENCE PAGE ON OUR WEBSITE :
@@ -134,15 +199,27 @@ def export_data(filename, tomo_data: TomoData):
         json.dump(dict, f)
 
 
+def get_fields_annotations(m: Type[BaseModel]) -> Dict[str, Any]:
+    default_annotations = get_type_hints(m)
+    return {
+        field_name: default_annotations[field_name] for field_name in m.model_fields
+    }
+
+
 def import_data(filename: str, config: Union[TomoConfiguration, None] = None):
     filepath = Path(filename)
     with open(filepath) as f:
         json_dict = json.load(f)
 
-    if config:
-        json_dict["config"] = config
-    data = TomoData.model_validate(json_dict)
+    data_fields = get_fields_annotations(TomoData)
+    for key, value in data_fields.items():
+        print(value)
+        if value is np.ndarray and key in json_dict:
+            print("casting")
+            cast_to_numpy(json_dict, key)
 
+    data = TomoData(**json_dict)
+    print(data)
     return data
 
 
